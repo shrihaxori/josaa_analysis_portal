e64ad101b43bcd3cc69e3956fa747947b32f6418
shrihaxori <ashrihariraju03@gmail.com>
2023-07-02 00:42:05 +0530

    database changes

--- website/portal/models.py
+++ website/portal/models.py
@@ -2,22 +2,13 @@ from django.db import models
 
 # Create your models here.
 class College(models.Model):
-    program_name = models.CharField(max_length=200)
-    seat_type = models.BooleanField()
-    quota = models.CharField(max_length=50)
-    gender = models.CharField(max_length=50)
-    opening_rank = models.PositiveIntegerField(default=200000)
-    closing_rank = models.PositiveIntegerField(default=200000)
-    year = models.PositiveSmallIntegerField(default = 2022)
-    round = models.PositiveSmallIntegerField(default = 6)
-    def __str__(self):
-        return self.college_text
+    institute = models.CharField(max_length=200, default='NaN')
+    program_name = models.CharField(max_length=200, default='NaN')
+    quota = models.CharField(max_length=50, default='NaN')
+    seat_type = models.CharField(max_length=50, default='NaN')
+    gender = models.CharField(max_length=50, default='NaN')
+    opening_rank = models.CharField(max_length=50, default='NaN')
+    closing_rank = models.CharField(max_length=50, default='NaN')
+    year = models.CharField(max_length=50, default='NaN')
+    round = models.CharField(max_length=50, default='NaN')
 
-class User(models.Model):
-    mains_rank = models.IntegerField()
-    advanced_rank = models.IntegerField()
-    seat_type = models.BooleanField()
-    quota = models.CharField(max_length=50)
-    gender = models.CharField(max_length=50)
-    def __str__(self):
-        return self.user_text
